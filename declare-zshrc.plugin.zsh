b067b6f1d6791d8bd3fae56fec7caf04e174ee3d
Salvydas Lukosius <sall@w-ss.io>
2021-11-02 11:52:30 +0000

    new file:   .declzsh-usage 	new file:   .gitignore 	new file:   @declzsh-process-buffer 	new file:   LICENSE 	new file:   README.md 	new file:   declare-zshrc.plugin.zsh 	new file:   declzsh 	new file:   img/add.png 	new file:   img/purge.png 	new file:   img/toggle.png 	new file:   img/usage.png

---
+++ declare-zshrc.plugin.zsh
@@ -0,0 +1,11 @@
+# Handle $0 according to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+typeset -g DECLZSH_REPO_DIR="${0:h}"
+typeset -g DECLZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/declare-zshrc"
+
+autoload @declzsh-process-buffer declzsh .declzsh-usage
+
+alias declzsh='noglob declzsh'
