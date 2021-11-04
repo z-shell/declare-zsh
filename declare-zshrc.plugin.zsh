ae9a1c306cbb5514c2a97e6a00601aa49e72b104
Salvydas Lukosius <sall@digitalclouds.pro>
2021-11-04 11:14:24 +0000

    :white_check_mark:

--- declare-zshrc.plugin.zsh
+++ declare-zshrc.plugin.zsh
@@ -1,5 +1,5 @@
 # Handle $0 according to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
+# http://z-shell.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
